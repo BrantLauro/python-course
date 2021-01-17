04e1ed3ae58ea575d5d48b0b6212169fb2d31a59
Lauro Henrique Brant <brantlauro@gmail.com>
2021-01-17 20:37:03 -0300

    ex022

--- ex/ex022.py
+++ ex/ex022.py
@@ -1,9 +1,9 @@
-nome = str(input('Type your complete name: ')).strip()
+name = str(input('Type your complete name: ')).strip()
 
-print(f'Your name to upper case is {nome.upper()} \n'
-      f'Your name to lower case is {nome.lower()} \n'
-      f'Your complete name had {len(nome) - nome.count(" ")} letters on total')
+print(f'Your name to upper case is {name.upper()} \n'
+      f'Your name to lower case is {name.lower()} \n'
+      f'Your complete name had {len(name) - name.count(" ")} letters on total')
 
-nome = nome.split()
+name = name.split()
 
-print(f'Seu primeiro nome tem {len(nome[0])} letras no total')
+print(f'Your first name had {len(name[0])} letters on total')
