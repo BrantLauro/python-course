43d33af7f09c9b9827dd52c04d5fab4e7fe4ff5c
Lauro Henrique Brant <brantlauro@gmail.com>
2021-02-19 17:31:50 -0300

    Update ex039.py

--- module02/ex/ex039.py
+++ module02/ex/ex039.py
@@ -1,22 +1,21 @@
 from datetime import date
 
 birth = int(input('What is your year of birth: '))
-genre = int(input('What is your genre? (type 1 for male and 2 for female): ')
+genre = int(input('What is your genre? (type 1 for male and 2 for female): '))
 year =  date.today().year
 age = year - birth
-
+under = 18-age
+over = age-18
+ 
 if genre == 1:
-    under = 18-age
-    over = age-18
+    
     if age < 18:
         print(f'You will enlist in {under} years. In {year+under}')
     elif age == 18:
         print('Is time to enlist')
     else:
         print(f'Enlistment time has passed {over} years ago. In {year-over}')
-elif genre == 1:
-    under = 18-age
-    over = age-18
+elif genre == 2:
     if age < 18:
         print(f'Woman do not need to enlist but you can do it in {year+under} if you want to.')
     elif age == 18:
