fb9cb083eb7a906a4c380f32b9348f1e47c7f501
Lauro Henrique Brant <brantlauro@gmail.com>
2021-02-20 12:30:57 -0300

    Update ex041.py

--- module02/ex/ex041.py
+++ module02/ex/ex041.py
@@ -1,4 +1,8 @@
-swimmer_age = int(input('What is the swimmer age? '))
+from datetime import date
+
+swimmer_birth = int(input('What year the swimmer was born? '))
+date = date.today().year
+swimmer_age = date - swimmer_birth 
 
 if swimmer_age <= 9:
     print(f'With {swimmer_age} years old you fit in the child category')
@@ -6,7 +10,7 @@ elif swimmer_age <= 14:
     print(f'With {swimmer_age} years old you fit in the infant category')
 elif swimmer_age <= 19:
     print(f'With {swimmer_age} years old you fit in the junior category')
-elif swimmer_age <= 20:
+elif swimmer_age <= 25:
     print(f'With {swimmer_age} years old you fit in the senior category')
 else:
     print(f'With {swimmer_age} years old you fit in the master category')
