37b6e080d33c07a796c51bccf95488783d8c3a00
Lauro Henrique Brant <brantlauro@gmail.com>
2021-02-28 18:29:27 -0300

    Update ex058.py

--- module02/ex/ex058.py
+++ module02/ex/ex058.py
@@ -2,7 +2,7 @@ from random import randint
 from time import sleep
 
 print('-=-' * 22)
-print('-=- I will think in a number between 0 and 5. Try to guess... -=-')
+print('-=- I will think in a number between 0 and 10. Try to guess... -=-')
 print('-=-' * 22)
 
 n = int(input('What number I thought? '))
@@ -14,10 +14,16 @@ print('PROCESSING...')
 sleep(2)
 
 while n != r:
-     n = int(input(f'OH NO! You missed! Try again: '))
-     tries += 1
-     print('PROCESSING...')
-     sleep(2)
+    if n > r:
+        n = int(input(f'OH NO! You missed, The number is less! Try again: '))
+        tries += 1
+        print('PROCESSING...')
+        sleep(2)
+    elif n < r:
+        n = int(input(f'OH NO! You missed, The number is better! Try again: '))
+        tries += 1
+        print('PROCESSING...')
+        sleep(2)
 
 if n == r:
     print('CONGRATULATIONS! You won!')
