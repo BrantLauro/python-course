06f52ff8e5848c7a7aa334c535fea10ec8e8607c
Lauro Henrique Brant <brantlauro@gmail.com>
2021-03-05 19:41:49 -0300

    Update ex068.py

--- module02/ex/ex068.py
+++ module02/ex/ex068.py
@@ -1,8 +1,9 @@
 from random import randint
 n = c = 0
-choose = ''
-pc = randint(1, 10)
+
 while True:
+    choose = ''
+    pc = randint(1, 10)
     n = int(input('Type a number between 1 and 10: '))
     if 1 <= n <= 10:
         choose = input('Even or Odd? [E/O] ').strip().upper()[0]
@@ -10,20 +11,18 @@ while True:
             if (pc + n) % 2 == 0:
                 print(f'Pc choose {pc}. {pc + n} is Even! You won congrats!')
                 c += 1
-                pc = randint(1, 10)
             else:
-                print(f'Pc choose {pc}. {pc + n} is Odd! You lose, sorry!')
+                print(f'Pc choose {pc}. {pc + n} is Odd! You lost, sorry!')
                 break
         elif choose == 'O':
             if (pc + n) % 2 != 0:
                 print(f'Pc choose {pc}. {pc + n} is Odd! You won congrats!')
                 c += 1
-                pc = randint(1, 10)
             else:
-                print(f'Pc choose {pc}. {pc + n} is Even! You lose, sorry!')
+                print(f'Pc choose {pc}. {pc + n} is Even! You lost, sorry!')
                 break
         else:
-            print('[ERROR] Try Again')
+            print('[ERROR] Try Again!')
     else:
-        print('Type a number between 1 and 10')
+        print('Type a number between 1 and 10!')
 print(f'You won {c} times!')
