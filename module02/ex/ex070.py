78ebda624c830b6f58338f8fc073835583d22d34
Lauro Henrique Brant <brantlauro@gmail.com>
2021-03-05 20:59:38 -0300

    Create ex070.py

---
+++ module02/ex/ex070.py
@@ -0,0 +1,23 @@
+total = products_1000 = 0
+cheapest = ' '
+prices = []
+while True:
+    print('~'*11)
+    print('CHEAP STORE')
+    print('~'*11)
+    product = input('What is the product? ').strip()
+    price = int(input('What is the price? $'))
+    prices.append(price)
+    total += price
+    if min(prices) == price:
+        cheapest = product
+    if price >= 1000:
+        products_1000 +=1
+    choose = ' '
+    while choose not in 'YN':
+        choose = input('Want to continue? [Y/N] ').strip().upper()[0]
+    if choose == 'N':
+        break
+print(f'The total is ${total}')
+print(f'There is {products_1000} products more expansive than $1000')
+print(f'The cheapest product is {cheapest} that costs: ${min(prices)}')
