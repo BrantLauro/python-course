a673e0e0372ea808cb747672d8ea4330c0f71810
Lauro Henrique Brant <brantlauro@gmail.com>
2021-03-05 22:06:53 -0300

    Update ex071.py

---
+++ module02/ex/ex071.py
@@ -0,0 +1,23 @@
+print('='*18)
+print('LAURO_BRANT_S BANK')
+print('='*18)
+money = int(input('How many you want to withdraw? $'))
+total = money
+paper = 50
+total_paper = 0 
+while True:
+    if total >= paper:
+        total -= paper
+        total_paper += 1
+    else:
+        if total_paper != 0:
+            print(f'{total_paper} notes of {paper}')
+        if paper == 50:   
+            paper = 20
+        elif paper == 20:
+            paper = 10
+        elif paper == 10:
+            paper = 1
+        total_paper = 0
+        if total == 0:
+            break
