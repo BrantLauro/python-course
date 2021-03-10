be2c6bcb709c0a07252c736b20ec1e3868c76807
Lauro Henrique Brant <brantlauro@gmail.com>
2021-03-09 21:47:13 -0300

    Update class16a.py

--- module03/classes/class16a.py
+++ module03/classes/class16a.py
@@ -1,7 +1,10 @@
 lunch = 'hamburger', 'pizza', 'fries', 'milkshake'
 
-for c in lunch:
-    print(f'I will eat {c}!')
+#for c in lunch:
+#    print(f'I will eat {c}!')
 
 #for c in range(0, len(lunch)):
-#    print(f'I will eat {lunch[c]}!')
+#    print(f'I will eat {lunch[c]} in the position {c}!')
+
+for pos, c in enumerate(lunch):
+    print(f'I will eat {c} in the position {pos}')
