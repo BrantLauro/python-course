2add1ea1cf4f0fed3e1cf120df2c85fb79c57b15
Lauro Henrique Brant <brantlauro@gmail.com>
2021-04-20 21:44:08 -0300

    Create ex080.py

---
+++ module03/ex/ex080.py
@@ -0,0 +1,13 @@
+numbers = []
+for c in range(0, 5):
+    number = (int(input('Type a number: ')))
+    if c == 0 or number > numbers[-1]:
+        numbers.append(number)
+    else:
+        pos = 0
+        while pos < len(number):
+            if number <= numbers[pos]:
+                numbers.insert(pos, number)
+                break
+            pos += 1
+print(numbers)
