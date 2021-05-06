1477f2d087775d80094f199e0cdda63363dfa803
Lauro Brant <brantlauro@gmail.com>
2021-05-05 21:53:27 -0300

    create ex088.py

---
+++ module03/ex/ex088.py
@@ -0,0 +1,26 @@
+from random import randint as rd
+from time import sleep as sl
+
+def lin():
+    print('='*30)
+lin()
+print('          Mega-Sena       ')
+lin()
+games = []
+times = int(input('How many games do you want to play? '))
+
+for c in range(0, times):    
+    games.append([])
+
+    while len(games[c]) < 6:
+        number = rd(1, 60)
+        if number not in games[c]:
+            games[c].append(number)
+
+    games[c].sort()
+    sl(0.1)
+    print(games[c])
+
+lin()
+print('          Good Luck      ')
+lin()
