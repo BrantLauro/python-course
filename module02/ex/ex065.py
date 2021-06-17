33d6b3e4c6e93ff535e88672a610544d33616abf
Lauro Brant <brantlauro@gmail.com>
2021-06-17 15:58:31 -0300

    update

--- module02/ex/ex065.py
+++ module02/ex/ex065.py
@@ -1,9 +1,9 @@
 n = 0
-stop = 'S'
+stop = 'Y'
 numlist = []
 while stop != 'N':
     n = int(input('Type a number: '))
-    stop = input('What to continue [S/N]? ').upper().strip()[0]
+    stop = input('What to continue [Y/N]? ').upper().strip()[0]
     numlist.append(n)
 print(f'The Average of the {len(numlist)} numbers typed is {sum(numlist)/len(numlist)}')
 print(f'The better is {max(numlist)} the lower is {min(numlist)}')
